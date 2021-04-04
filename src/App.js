28552429a857e7077d11dd082343443e2310a3e2
Ankit Abhinav <ankit.abhinav19@gmail.com>
2021-04-04 21:08:43 +0530

    added portfolio website cra template

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Portfolio website coming soon
         </a>
       </header>
     </div>
